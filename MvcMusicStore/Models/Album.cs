3909a9474f7b3fb2aadafbbd4cbc773948cb1f96
sonicpro <sonic@ukrpost.net>
2014-05-11 21:35:16 +0300

    Album model added.

--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -13,7 +13,20 @@ namespace MvcMusicStore.Models
 		public virtual string Title { get; set; }
 		public virtual decimal Price { get; set; }
 		public virtual string AlbumArtUrl { get; set; }
-		public virtual Artist Genre { get; set; }
+		public virtual Artist Artist { get; set; }
 		public virtual Genre Genre { get; set; }
+	}
+
+	public class Artist
+	{
+		public virtual int ArtistId { get; set; }
+		public virtual string Name { get; set; }
+	}
+
+	public class Genre
+	{
+		public virtual int GenreId { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string Description { get; set; }
 	}
 }
